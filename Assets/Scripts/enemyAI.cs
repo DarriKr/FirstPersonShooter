a4321b4a7c038e2518c30922fbbfe8878c43b8ef
DarriKr <darrikristjansson@gmail.com>
2021-06-24 15:58:20 +0000

    7. Enemy AI chase range added

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -6,18 +6,23 @@ using UnityEngine.AI;
 public class enemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
-    NavMeshAgent NavMeshAgent;
-    
+    [SerializeField] float chaseRange = 5f;
 
-    // Start is called before the first frame update
+    NavMeshAgent NavMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+
     void Start()
     {
         NavMeshAgent = GetComponent<NavMeshAgent>();    
     }
 
-    // Update is called once per frame
     void Update()
     {
-        NavMeshAgent.SetDestination(target.position);    
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= chaseRange)
+        {
+            NavMeshAgent.SetDestination(target.position);
+        } 
     }
 }
