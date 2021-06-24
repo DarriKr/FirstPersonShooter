dd6db8f34f076a1efae2a2eed5cc32d88d596c7a
DarriKr <darrikristjansson@gmail.com>
2021-06-24 15:10:31 +0000

    5. Used NavMeshAgent For AI

---
+++ Assets/Scripts/enemyAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class enemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent NavMeshAgent;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavMeshAgent = GetComponent<NavMeshAgent>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavMeshAgent.SetDestination(target.position);    
+    }
+}
