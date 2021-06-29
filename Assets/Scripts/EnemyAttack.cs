6609f0b30b7908ef90b44ccf57e680b39f8a0fbe
DarriKr <darrikristjansson@gmail.com>
2021-06-29 10:23:04 +0000

    24. Added weapon zoom feature

--- Assets/Scripts/EnemyAttack.cs
+++ Assets/Scripts/EnemyAttack.cs
@@ -12,11 +12,6 @@ public class EnemyAttack : MonoBehaviour
         target = FindObjectOfType<PlayerHealth>();
     }
 
-    public void OnDamageTaken()
-    {
-        Debug.Log(name + " I also know that we took damage");
-    }
-
     public void AttackHitEvent()
     {
         if (target == null) return;
